6049f01ea91eaf68b734ce91208905ca9a13af49
Joel Schwegmann <schwegmann@hob-by-horse.de>
2025-03-28 05:13:43 +0100

    implement glb rendering

--- src/plugins/renderEngine.ts
+++ src/plugins/renderEngine.ts
@@ -2,6 +2,11 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { KMZLoader } from 'three/addons/loaders/KMZLoader.js';
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import modelDefault from '../assets/tile_default.glb'
 
 export default class renderEngine {
 
@@ -11,10 +16,20 @@ export default class renderEngine {
     renderer;
     light;
     controls;
-    floorMaterial;
-    floor;
+    config;
+
+    model:any;
 
     constructor(id: string) {
+
+        this.config = {
+            tileSize: 0.395,
+            showGrid: true,
+            showAxes: true,
+            tileFactor: 0.005,
+            testXMeters: 15.5,
+            testYMeters: 20.20
+        }
 
         this.container = document.querySelector(id);
         this.camera = new THREE.PerspectiveCamera( 60, this.container!.clientWidth / this.container!.clientHeight, 1, 100 );
@@ -23,16 +38,13 @@ export default class renderEngine {
         this.light = new THREE.DirectionalLight( 0xffffff, 3 );
 
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-
-        this.floorMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} )
-        this.floor = new THREE.Mesh(new THREE.PlaneGeometry( 1, 1 ), this.floorMaterial );
     }
     createView():void {
         this.renderer.setSize(this.container!.clientWidth, this.container!.clientHeight);
         this.scene.background = new THREE.Color('white');
 
         // creating light
-        this.light.position.set( 0.5, 1.0, 0.5 ).normalize();
+        this.light.position.set( 0.5, .75, 0.5 ).normalize();
         this.scene.add( this.light );
 
         // set camera
@@ -44,13 +56,16 @@ export default class renderEngine {
         this.renderer.setSize( this.container!.clientWidth, this.container!.clientHeight );
         this.container!.appendChild( this.renderer.domElement );
 
-        const grid = new THREE.GridHelper( 50, 50, 0xffffff, 0x7b7b7b );
-        this.scene.add( grid );
+        this.addTiles()
 
-        const hlp = new THREE.AxesHelper(1);
-        this.scene.add(hlp);
-
-        this.createInitFloor()
+        if (this.config.showGrid) {
+            const grid = new THREE.GridHelper( 50, 50, 0xffffff, 0x7b7b7b );
+            this.scene.add( grid );
+        }
+        if (this.config.showAxes) {
+            const hlp = new THREE.AxesHelper(1);
+            this.scene.add(hlp);
+        }
 
         // adding controls
         this.controls.addEventListener( 'change', () => {
@@ -66,14 +81,6 @@ export default class renderEngine {
 
         this.renderer.render(this.scene, this.camera)
     }
-    createInitFloor():void {
-        this.floor.rotateX(Math.PI / 180 * 90);
-        this.scene.add( this.floor );
-    }
-    updateFloor(x: number, y: number): void {
-        console.log(x)
-        console.log(y)
-    }
     onWindowResize():void {
 
         this.camera.aspect = this.container!.clientWidth / this.container!.clientHeight;
@@ -86,5 +93,42 @@ export default class renderEngine {
     render():void {
         this.renderer.clear();
         this.renderer.render( this.scene, this.camera );
+    }
+    addTiles():void {
+        /*
+        TEESTTTING
+        */
+        const loader = new GLTFLoader();
+
+        const timesColumn = Math.floor(this.config.testXMeters / this.config.tileSize)
+        const timesRow = Math.floor((this.config.testYMeters - 1) / this.config.tileSize)
+
+        console.log(timesColumn)
+
+        const allGroup = new THREE.Group();
+
+        for (let x = 0; x < timesColumn; x++) {
+            let _this = this;
+            for (let y = 0; y < timesRow; y++) {
+                loader.load(
+                    modelDefault,
+                    (glb:any) => {
+                        glb.scene.scale.set(this.config.tileFactor, this.config.tileFactor, this.config.tileFactor)
+                        glb.scene.position.set(x, 1, (y + 1))
+                        _this.model = glb.scene;
+                        allGroup.add(_this.model)
+                    },
+                    function (xhr:ProgressEvent) {
+                        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+                    },
+                    function (error:any) {
+                        console.log("An error happened:", error);
+                    }
+                );
+            }
+        }
+
+        allGroup.updateMatrixWorld( true )
+        this.scene.add(allGroup)
     }
 }
